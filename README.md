b1f55ec262b3fd0e75e36f15b59edc0967e65157
Alex Via <alexviacoll@gmail.com>
2025-10-26 18:03:37 +0100

    Disable terminal canonical mode and echo and start processing user input

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ character.
 The ASCII escape character, octal: `\033`, hexadecimal: `\x1b`, decimal: `27`,
 also represented as `^[` or `\e` is used to start a control sequence.
 
-Tip: `cat -v` is extremely useful for debugging non-printable characters:
+💡Tip: `cat -v` is extremely useful for debugging non-printable characters:
 ```shell
 $ ls --color | cat -v
 ```
@@ -19,6 +19,32 @@ printf("\x1b[%uD", n); // Move the cursor n cells back
 
 printf("\x1b[%u;%uH", row, col); // Move the cursor to row, col
 ```
+
+## Function and Extended key escape sequences
+| Key    | Sequence    | Bytes (hex)    |
+| ------ | ----------- | -------------- |
+| F1     | ESC O P     | 1B 4F 50       |
+| F2     | ESC O Q     | 1B 4F 51       |
+| F3     | ESC O R     | 1B 4F 52       |
+| F4     | ESC O S     | 1B 4F 53       |
+| F5     | ESC [ 1 5 ~ | 1B 5B 31 35 7E |
+| F6     | ESC [ 1 7 ~ | 1B 5B 31 37 7E |
+| F7     | ESC [ 1 8 ~ | 1B 5B 31 38 7E |
+| F8     | ESC [ 1 9 ~ | 1B 5B 31 39 7E |
+| F9     | ESC [ 2 0 ~ | 1B 5B 32 30 7E |
+| F10    | ESC [ 2 1 ~ | 1B 5B 32 31 7E |
+| F11    | ESC [ 2 3 ~ | 1B 5B 32 33 7E |
+| F12    | ESC [ 2 4 ~ | 1B 5B 32 34 7E |
+| ↑      | ESC [ A     | 1B 5B 41       |
+| ↓      | ESC [ B     | 1B 5B 42       |
+| →      | ESC [ C     | 1B 5B 43       |
+| ←      | ESC [ D     | 1B 5B 44       |
+| Home   | ESC [ H     | 1B 5B 48       |
+| End    | ESC [ F     | 1B 5B 49       |
+| Insert | ESC [ 2 ~   | 1B 5B 32 7E    |
+| Delete | ESC [ 3 ~   | 1B 5B 33 7E    |
+| PgUp   | ESC [ 5 ~   | 1B 5B 35 7E    |
+| PgDown | ESC [ 6 ~   | 1B 5B 36 7E    |
 
 ## References
 - https://en.wikipedia.org/wiki/ANSI_escape_code
--- main.c
+++ main.c
@@ -61,44 +61,61 @@ int main(void)
 
     get_terminal_size(&rows, &cols);
 
-    struct termios oldt;
+    struct termios oldt, newt;
     if (tcgetattr(STDIN_FILENO, &oldt) == -1) {
         fprintf(stderr, "ERROR: Unable to get terminal attributes\n");
         return 1;
     }
-    printf("termios:\n");
-    printf("  c_iflag: 0x%x\n", oldt.c_iflag);
-    printf("  c_oflag: 0x%x\n", oldt.c_oflag);
-    printf("  c_cflag: 0x%x\n", oldt.c_cflag);
-    printf("  c_lflag: 0x%x\n", oldt.c_lflag); // IEXTEN|ECHOKE|ECHOCTL|ECHOK|ECHOE|ECHO|ICANON|ISIG
-    printf("  ISIG:   0x%x\n", ISIG);
-    printf("  ICANON: 0x%x\n", ICANON);
-    printf("  XCASE:  0x%x\n", XCASE);
-    printf("  ECHO:   0x%x\n", ECHO);
-    printf("  ECHOE:  0x%x\n", ECHOE);
-    printf("  ECHOK:  0x%x\n", ECHOK);
-    printf("  ECHONL: 0x%x\n", ECHONL);
-    printf("  ECHOCTL:0x%x\n", ECHOCTL);
-    printf("  ECHOPRT:0x%x\n", ECHOPRT);
-    printf("  ECHOKE: 0x%x\n", ECHOKE);
-    printf("  FLUSHO: 0x%x\n", FLUSHO);
-    printf("  NOFLSH: 0x%x\n", NOFLSH);
-    printf("  TOSTOP: 0x%x\n", TOSTOP);
-    printf("  PENDIN: 0x%x\n", PENDIN);
-    printf("  IEXTEN: 0x%x\n", IEXTEN);
-    exit(0);
+
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO | ISIG);
+    newt.c_cc[VMIN]  = 0; // Minimum number of characters to read
+    newt.c_cc[VTIME] = 1; // Timeout in deciseconds for noncanonical read
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &newt) == -1) {
+        fprintf(stderr, "ERROR: Unable to set terminal attributes\n");
+        return 1;
+    }
 
     printf("\x1b[?25l"); // Hide cursor
-    printf("\x1b]0;New title\x07");
+    printf("\x1b]0;Pong\x07");
 
-    while (1) {
+    int x = 0;
+    int y = 0;
+    while (true) {
+        // Poll input
+        char c;
+        ssize_t res = read(STDIN_FILENO, &c, 1);
+        if (res == 1) {
+            if (c == 0x1b) {
+                printf("0x%x", c);
+                while (read(STDIN_FILENO, &c, 1) == 1) {
+                    printf(" 0x%x", c);
+                }
+                printf("\n");
+            } else {
+                if (c == 0x03)      printf("INTR  Ctrl-C\n");
+                else if (c == 0x1c) printf("QUIT  Ctrl-\\\n");
+                else if (c == 0x1a) printf("SUSP  Ctrl-Z\n");
+                else if (c == 0x19) printf("DSUSP Ctrl-Y\n");
+                else {
+                    printf("read: %c 0x%x\n", c, c);
+                    if (c == 'q') break;
+                    if (c == 'h') x -= 1;
+                    if (c == 'l') x += 1;
+                    if (c == 'j') y += 1;
+                    if (c == 'k') y -= 1;
+                }
+            }
+        }
+
+        // Render
         printf("\x1b[2J"); // Clear entire screen
         printf("\x1b[H");  // Move cursor to top-left
 
         // Clear background
-        uint8_t r = 80;
-        uint8_t g = 0;
-        uint8_t b = 100;
+        uint8_t r = 10;
+        uint8_t g = 10;
+        uint8_t b = 10;
         printf("\x1b[48;2;%u;%u;%um", r, g, b); // Set background color
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
@@ -106,49 +123,20 @@ int main(void)
             }
         }
 
-        printf("\x1b[48;2;%u;%u;%um", 0, 100, 0); // Set background color
-        printf("\x1b[%u;%uH", 0, 0); // Move cursor to next row
+        printf("\x1b[48;2;%u;%u;%um", 111, 111, 111); // Set background color
+        printf("\x1b[%u;%uH", y+1, x+1); // Move cursor to row: y, col: x
         for (int row = 0; row < 10; row++) {
             for (int col = 0; col < 10; col++) {
                 printf(" ");
             }
-            printf("\x1b[%u;%uH", row, 0); // Move cursor to next row
+            printf("\x1b[%u;%uH", y+1+row, x+1); // Move cursor to next row
         }
 
         fflush(stdout);
-        sleep(1);
     }
 
-    return 0;
-
-    //printf("Hello");
-    //printf("\x1b[%uD", 5); // Move cursor 5 cells back
-    //printf("Good bye\n");
-
-    //printf("\x1b[%u;%uH", 10, 10); // Move cursor to row 10, column 10
-    //printf("Hello\n");
-
-    //printf("\x1b[0J"); // Clear from cursor to end of the screen
-    //printf("\x1b[1J"); // Clear from cursor to beginning of the screen
-    //printf("\x1b[2J"); // Clear entire screen
-    //printf("\x1b[3J"); // Clear entire screen and delete lines saved in scrollback buffer
-
-    //printf("\x1b[%u;%uH", 1, 1); // Move cursor to row 1, column 1
-
-    for (int color = 0; color <= 7; color++) {
-        printf("ESC[3%um: ", color);
-        printf("\x1b[3%umHello\x1b[0m\n", color); // Set foreground color
-    }
-
-    for (int color = 0; color <= 7; color++) {
-        printf("ESC[4%um: ", color);
-        printf("\x1b[4%umHello\x1b[0m\n", color); // Set background color
-    }
-
-    //printf("\x1b[999;999H");
-    //printf("\x1b[6n"); // Reports the cursor position (CPR) by transmitting ESC[n;mR, where n is the row and m the column
-
-    //printf("\x1b[38;2;255;0;0mHello\n\x1b[0m");
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    handle_int(0);
 
     return 0;
 }
